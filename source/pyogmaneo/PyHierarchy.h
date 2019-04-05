14d57deb1754a54b3b7baaa44603dcb0a6ee29c0
Eric Laukien <ericlaukien@gmail.com>
2019-04-05 13:17:24 -0400

    Fixes

--- source/pyogmaneo/PyHierarchy.h
+++ source/pyogmaneo/PyHierarchy.h
@@ -95,6 +95,14 @@ namespace pyogmaneo {
             return _h.getSCLayer(l)._alpha;
         }
 
+        void setSCBeta(int l, float beta) {
+            _h.getSCLayer(l)._beta = beta;
+        }
+
+        float getSCBeta(int l) const {
+            return _h.getSCLayer(l)._beta;
+        }
+
         void setSCRefractoryTicks(int l, int refractoryTicks) {
             _h.getSCLayer(l)._refractoryTicks = refractoryTicks;
         }
