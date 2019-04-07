12952e2c9a58ee01ba0fdad99f8d8366d7239703
Eric Laukien <ericlaukien@gmail.com>
2019-04-07 11:38:17 -0400

    Separated state

--- examples/sineWaveExample.py
+++ examples/sineWaveExample.py
@@ -13,7 +13,7 @@ import pyogmaneo
 import matplotlib.pyplot as plt
 
 # Create the compute system using a device
-cs = pyogmaneo.PyComputeSystem(4)
+cs = pyogmaneo.PyComputeSystem(16)
 
 # This defines the resolution of the input encoding - we are using a simple single column that represents a bounded scalar through a one-hot encoding. This value is the number of "bins"
 inputColumnSize = 32
@@ -24,11 +24,11 @@ bounds = (-1.0, 1.0)
 # Define layer descriptors: Parameters of each layer upon creation
 lds = []
 
-for i in range(5): # 5 layers with exponential memory
+for i in range(9): # 9 layers with exponential memory
     ld = pyogmaneo.PyLayerDesc()
 
     # Set the hidden (encoder) layer size: width x height x columnSize
-    ld._hiddenSize = pyogmaneo.PyInt3(5, 5, 16)
+    ld._hiddenSize = pyogmaneo.PyInt3(4, 4, 16)
 
     ld._scRadius = 2 # Sparse coder radius onto visible layers
     ld._pRadius = 2 # Predictor radius onto sparse coder hidden layer (and feed back)
@@ -41,24 +41,26 @@ for i in range(5): # 5 layers with exponential memory
 # Create the hierarchy: Provided with input layer sizes (a single column in this case), and input types (a single predicted layer)
 h = pyogmaneo.PyHierarchy(cs, [ pyogmaneo.PyInt3(1, 1, inputColumnSize) ], [ pyogmaneo._inputTypePred ], lds)
 
+state = pyogmaneo.PyState(h)
+
 # After creation, we can set run-time parameters
 for i in range(len(lds)):
     # Encoder alpha
-    h.setSCAlpha(i, 0.1)
+    h.setSCAlpha(i, 0.01)
 
     for j in range(h.getNumVisibleLayers(i)):
         if h.visibleLayerExists(i, j):
-            h.setPAlpha(i, j, 0.5)
+            h.setPAlpha(i, j, 1.0)
 
 # Present the wave sequence for some timesteps
-iters = 3000
+iters = 5000
 
 for t in range(iters):
     # The value to encode into the input column
-    valueToEncode = np.sin(t * 0.02 * 2.0 * np.pi) * np.sin(t * 0.035 * 2.0 * np.pi + 0.45)
+    valueToEncode = np.sin(t * 0.1 * 2.0 * np.pi) * np.sin(t * 0.08 * 2.0 * np.pi + 0.45)# * np.sin(t * 0.02 * 2.0 * np.pi) * np.sin(t * 0.035 * 2.0 * np.pi + 0.45)
 
     # Step the hierarchy given the inputs (just one here)
-    h.step(cs, [ [ int((valueToEncode - bounds[0]) / (bounds[1] - bounds[0]) * (inputColumnSize - 1) + 0.5) ] ], True) # True for enabling learning
+    h.step(cs, [ [ int((valueToEncode - bounds[0]) / (bounds[1] - bounds[0]) * (inputColumnSize - 1) + 0.5) ] ], state, True) # True for enabling learning
 
     # Print progress
     if t % 100 == 0:
@@ -73,10 +75,10 @@ for t in range(300):
     t2 = t + iters # Continue where previous sequence left off
 
     # New, continued value for comparison to what the hierarchy predicts
-    valueToEncode = np.sin(t2 * 0.02 * 2.0 * np.pi) * np.sin(t2 * 0.035 * 2.0 * np.pi + 0.45)
+    valueToEncode = np.sin(t2 * 0.1 * 2.0 * np.pi) * np.sin(t2 * 0.08 * 2.0 * np.pi + 0.45)# * np.sin(t2 * 0.02 * 2.0 * np.pi) * np.sin(t2 * 0.035 * 2.0 * np.pi + 0.45)
 
     # Run off of own predictions with learning disabled
-    h.step(cs, [ h.getPredictionCs(0) ], False)
+    h.step(cs, [ h.getPredictionCs(0) ], state, False)
 
     # Retrieve the predicted column index
     predIndex = h.getPredictionCs(0)[0] # First (only in this case) input layer prediction
@@ -95,5 +97,3 @@ for t in range(300):
 # Show plot
 plt.plot(ts, vs, ts, trgs)
 plt.show()
-
-
