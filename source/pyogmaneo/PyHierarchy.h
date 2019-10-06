ee1d5e26d2c72bc11c3d3854804b5763e69b8da8
Eric Laukien <ericlaukien@gmail.com>
2019-10-05 21:11:41 -0400

    Fixes

--- source/pyogmaneo/PyHierarchy.h
+++ source/pyogmaneo/PyHierarchy.h
@@ -91,13 +91,13 @@ namespace pyogmaneo {
             return _h._alpha;
         }
 
-        // void setBeta(float beta) {
-        //     _h._beta = beta;
-        // }
+        void setBeta(float beta) {
+            _h._beta = beta;
+        }
 
-        // float getBeta() const {
-        //     return _h._beta;
-        // }
+        float getBeta() const {
+            return _h._beta;
+        }
 
         void setGamma(float gamma) {
             _h._gamma = gamma;
