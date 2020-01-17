01fe10bf379c6fd92acc84465deab249f8155c22
Eric Laukien <ericlaukien@gmail.com>
2020-01-17 11:19:23 -0500

    SARSA

--- source/pyogmaneo/PyHierarchy.h
+++ source/pyogmaneo/PyHierarchy.h
@@ -223,23 +223,6 @@ namespace pyogmaneo {
             return h.getALayers()[v]->alpha;
         }
 
-        void setABeta(
-            int v,
-            float beta
-        ) {
-            assert(h.getALayers()[v] != nullptr);
-            
-            h.getALayers()[v]->beta = beta;
-        }
-
-        float getABeta(
-            int v
-        ) const {
-            assert(h.getALayers()[v] != nullptr);
-            
-            return h.getALayers()[v]->beta;
-        }
-
         void setAGamma(
             int v,
             float gamma
